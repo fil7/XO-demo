04e5e78e58905c74634ba8c3c1793a3908784dad
fil757 <filonova7@yandex.ru>
2016-02-12 16:25:59 +0600

    refactoring field to be size flexible

--- src/ru/fil7/demo/xo/model/Field.java
+++ src/ru/fil7/demo/xo/model/Field.java
@@ -6,14 +6,20 @@ import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
+    private final int fieldSize;
     private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE = FIELD_SIZE;
+    //private final int MAX_COORDINATE = fieldSize;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    private Figure[][] field;
+
+    public Field(int fieldSize) {
+        this.fieldSize = fieldSize;
+        field = new Figure[fieldSize][fieldSize];
+    }
 
     public int getSize() {
-        return FIELD_SIZE;
+
+        return fieldSize;
     }
 
     public Figure getFigure(Point point) throws InvalidPointException {
@@ -32,10 +38,10 @@ public class Field {
     }
 
     private boolean checkPoint(final Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
 }
